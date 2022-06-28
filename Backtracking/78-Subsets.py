1d0f980f9873405d8b2fc80f8e65f91bcec4e7d7
Wisteli-ShikhaPandey <85352745+Shikha-code36@users.noreply.github.com>
2022-06-28 20:27:00 +0530

    Subsets

---
+++ Backtracking/78-Subsets.py
@@ -0,0 +1,31 @@
+'''
+                              Subsets    
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+'''
+def subsets(nums):
+    res = []
+
+    subset = []
+
+    def dfs(i):
+        if i >= len(nums):
+            res.append(subset.copy())
+            return
+        #decision to include nums[i]
+        subset.append(nums[i])
+        dfs(i + 1)
+        #decision NOT to include nums[i]
+        subset.pop()
+        dfs(i + 1)
+
+    dfs(0)
+    return res
+
+#T :O(2^n)
+#S :O(n)
--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 - [x] [Backtracking](Backtracking)
     - [x] [Word Search](Backtracking/79-Word-Search.py)
     - [x] [Letter Case Permutation](Backtracking/784-letter-case-permutation.py)
+    - [x] [Subsets](Backtracking/78-Subsets.py)
     
