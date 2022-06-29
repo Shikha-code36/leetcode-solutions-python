ebcad0bfbafab4fbdb431c05f0db60141a0b789a
Wisteli-ShikhaPandey <85352745+Shikha-code36@users.noreply.github.com>
2022-06-29 21:44:38 +0530

    Subsets-II

---
+++ Backtracking/90-Subsets-II.py
@@ -0,0 +1,39 @@
+'''
+                                    Subsets II
+Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+'''
+
+
+def subsetsWithDup(nums):
+    res = []
+
+    nums.sort()
+
+    def backtrack(i, subset):
+        if i == len(nums):
+            res.append(subset[::])
+            return
+
+         # All subsets that include nums[i]
+        subset.append(nums[i])
+        backtrack(i+1, subset)
+        subset.pop()
+
+        # All subsets that don't include nums[i]
+        while i+1 < len(nums) and nums[i] == nums[i+1]:
+            i += 1
+        backtrack(i+1, subset)
+    backtrack(0, [])
+    return res
+
+#T:O(n.2^n)
+#S:O(n)
--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # leetcode-solutions-python
+Python solutions to the leetcode problems
 
+## Notes
+Check the notes for the explaination - [Notes](https://stingy-shallot-4ea.notion.site/SeanPrashad-Leetcode-Patterns-7b700a3aa52543a4ace4dbdeb718b8cc)
+
+## Coding solution
 - [x] [Backtracking](Backtracking)
     - [x] [Word Search](Backtracking/79-Word-Search.py)
     - [x] [Letter Case Permutation](Backtracking/784-letter-case-permutation.py)
     - [x] [Subsets](Backtracking/78-Subsets.py)
+    - [x] [Subsets-II](Backtracking/78-Subsets.py)
     
